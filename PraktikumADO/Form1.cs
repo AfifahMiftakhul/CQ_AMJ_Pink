c7d29c539411deb7abb029f105ec6988c7013600
AfifahMiftakhul <apipiyahmiftakhul@gmail.com>
2026-03-12 16:50:06 +0700

    koneksi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -6,7 +6,22 @@ namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
-        
+        SqlConnection conn;
+        SqlCommand cmd;
+        public Form1()
+        {
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Koneksi();
+                conn.Open();
+
+                MessageBox.Show("Koneksi Berhasil");
+                conn.Close();
+            }
             catch (Exception ex) {
 
         }
